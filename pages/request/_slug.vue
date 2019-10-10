1383219fe4ce36fb523349122c31e40f2ea571c8
Scott Schaefer <sschaefer@rms.com>
2019-10-09 19:03:08 -0600

    get params

---
+++ pages/request/_slug.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container mx-auto px-4">
+    <div class="screenshot"></div>
+    <div class="form-container">
+      <form>
+        <input
+          id="comments"
+          class="form-comments"
+          type="text"
+          name="comments"
+        />
+        <input type="submit" value="approved" class="approve" />
+        <input type="submit" value="disapproved" class="disapprove" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ route: { params } }) {
+    console.log({ params })
+  }
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
